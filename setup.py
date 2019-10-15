6783ef2fe93350557cd342c8955d84f56679a331
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2019-10-15 14:21:59 +0200

    Firt release

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+README = open(os.path.join(HERE, 'README.md')).read()
+CHANGES = open(os.path.join(HERE, 'CHANGES.md')).read()
+
+
+setup(
+    name='django-token-tools',
+    version=open(os.path.join(HERE, 'token_tools', 'VERSION.md')).read().strip(),
+    include_package_data=True,
+    author="Makina Corpus",
+    author_email="terralego-pypi@makina-corpus.com",
+    description='Suite of Django tools around token authentication.',
+    long_description=README + '\n\n' + CHANGES,
+    description_content_type="text/markdown",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    url='https://github.com/Terralego/django-token-tools.git',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    install_requires=[
+        'django>=2.1,<3.0',
+    ],
+)
---
+++ token_tools/generator.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+
+from django.conf import settings
+from django.utils.crypto import constant_time_compare, salted_hmac
+from django.utils.http import base36_to_int, int_to_base36, urlsafe_base64_decode
+
+
+class TokenGenerator:
+    """
+    Generic token generation based on django.contrib.auth.tokens
+    """
+
+    key_salt = "django.contrib.auth.tokens.PasswordResetTokenGenerator"
+    secret = settings.SECRET_KEY
+
+    @property
+    def timestamp(self):
+        return int((datetime.now() - datetime(2001, 1, 1)).total_seconds())
+
+    def make_hash_string(self, *args, **kwargs):
+        return salted_hmac(
+            self.key_salt, self._make_hash_value(*args, **kwargs), secret=self.secret
+        ).hexdigest()[::2]
+
+    def make_token_with_timestamp(self, timestamp, *args, **kwargs):
+        ts_b36 = int_to_base36(timestamp)
+        hash_string = self.make_hash_string(*args, **kwargs)
+        return "%s-%s" % (ts_b36, hash_string)
+
+    def make_token(self, *args, **kwargs):
+        # timestamp is number of seconds since 2001-1-1. Converted to base 36,
+        # this gives us a 6 digit string until about 2069.
+        ts_b36 = int_to_base36(self.timestamp)
+        hash_string = self.make_hash_string(*args, **kwargs)
+        return "%s-%s" % (ts_b36, hash_string)
+
+    def decode_idb64(self, idb64):
+        try:
+            return urlsafe_base64_decode(idb64).decode()
+        except (UnicodeDecodeError, ValueError):
+            return None
+
+    def token_idb64(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def _make_hash_value(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def check_token(self, token, *args, **kwargs):
+        """
+        Check that the token is valid
+        """
+
+        if not token:
+            return False
+
+        try:
+            ts_b36, _ = token.split("-")
+            ts = base36_to_int(ts_b36)
+        except (ValueError, AssertionError):
+            return False
+
+        # Check that the timestamp/uid has not been tampered with
+        if not constant_time_compare(
+            self.make_token_with_timestamp(ts, *args, **kwargs), token
+        ):
+            return False
+
+        # Check the timestamp is within limit.
+        if (self.timestamp - ts) > settings.TOKEN_TIMEOUT:
+            return False
+
+        return True
