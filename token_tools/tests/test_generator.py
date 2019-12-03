d190373f060b3ef1d0119e482528a5c1e0c1e30d
J-E Castagnede <j.e.castagnede@gmail.com>
2019-12-03 15:03:36 +0100

    add test project

---
+++ token_tools/tests/test_generator.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from token_tools.generator import TokenGenerator
+
+
+class TokenGeneratorTestCase(TestCase):
+    def setup(self):
+        self.generator = TokenGenerator()
+
+    def test_fake(self):
+        # just test if module is loaded
+        self.assertTrue(True)
