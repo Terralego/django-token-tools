ac1b8ad3364ea8bd626927753f69353749173c45
J-E Castagnede <j.e.castagnede@gmail.com>
2019-12-03 15:36:27 +0100

    fix test project

--- test_token_tools/settings.py
+++ test_token_tools/settings.py
@@ -73,6 +73,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'test_token_tools.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
