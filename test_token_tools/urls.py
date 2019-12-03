455ad4ea2af5e69e954a990e7c7a888e45cb3f38
J-E Castagnede <j.e.castagnede@gmail.com>
2019-12-03 15:11:54 +0100

    fix test project

--- test_token_tools/urls.py
+++ test_token_tools/urls.py
@@ -1,4 +1,4 @@
-"""test_terra_accounts URL Configuration
+"""test_token_tools URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
